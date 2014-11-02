68849faa276158253aae3f07fcc68a022560adff
sgm <sim0629@gmail.com>
2014-11-02 19:41:48 +0900

    잘 되는 것 같음

--- evaluate.ml
+++ evaluate.ml
@@ -8,35 +8,50 @@ module Evaluator =
 struct
   exception Error of string
 
-  let rec rename used exp =
-    let rec rename_in name =
-      if List.mem name used then rename_in (name ^ "s")
-      else name
+  let rename exp =
+    let n = ref 0 in
+    let rec rename_in exp env =
+      match exp with
+      | Lambda.Id x -> (
+        let n = env x in
+        match n with
+        | None -> exp
+        | Some i -> Lambda.Id (string_of_int i)
+      )
+      | Lambda.Lam (x, e) -> (
+        let m = !n in
+        n := m + 1;
+        let env = fun y -> if y = x then Some m else env y in
+        Lambda.Lam (string_of_int m, rename_in e env)
+      )
+      | Lambda.App (l, r) -> (
+        Lambda.App (rename_in l env, rename_in r env)
+      )
     in
-    match exp with
-    | Lambda.Id x -> Lambda.Id (rename_in x)
-    | Lambda.Lam (x, e) -> Lambda.Lam (x, rename (List.filter (fun y -> y <> x) used) e)
-    | Lambda.App (l, r) -> Lambda.App (rename used l, rename used r)
+    rename_in exp (fun y -> None)
 
-  let rec replace name target source used =
+  let rec replace name target source =
     match target with
     | Lambda.Id x ->
-      if x = name then source
-      else target
+      if x = name then source else target
     | Lambda.Lam (x, e) ->
-      if x = name then target
-      else Lambda.Lam (x, replace name e (rename (x::used) source) (x::used))
+      Lambda.Lam (x, replace name e source)
     | Lambda.App (l, r) ->
-      Lambda.App (replace name l source used, replace name r source used)
+      Lambda.App (replace name l source, replace name r source)
 
-  let rec reduce : Lambda.lexp -> Lambda.lexp
-  = fun exp ->
+  let rec reduce_simple exp =
     match exp with
     | Lambda.App ((Lambda.Lam (name, target), source)) ->
-      reduce (replace name target source [])
+      reduce_simple (replace name target source)
     | Lambda.App (left, right) ->
-      Lambda.App (reduce left, reduce right)
-    | _ ->
-      exp
+      let fxp = Lambda.App (reduce_simple left, reduce_simple right) in
+      if exp = fxp then fxp
+      else reduce_simple fxp
+    | Lambda.Lam (name, exp) ->
+      Lambda.Lam (name, reduce_simple exp)
+    | _ -> exp
+
+  let reduce : Lambda.lexp -> Lambda.lexp
+  = fun exp -> reduce_simple (rename exp)
 
 end
