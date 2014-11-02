970978f9077e1e7e243c4b92103ccabf48dda932
sgm <sim0629@gmail.com>
2014-11-02 16:33:02 +0900

    이러면 되는 것 같은데

--- evaluate.ml
+++ evaluate.ml
@@ -8,7 +8,35 @@ module Evaluator =
 struct
   exception Error of string
 
-  let reduce : Lambda.lexp -> Lambda.lexp
-  = fun exp -> raise (Error "not implemented")
+  let rec rename used exp =
+    let rec rename_in name =
+      if List.mem name used then rename_in (name ^ "s")
+      else name
+    in
+    match exp with
+    | Lambda.Id x -> Lambda.Id (rename_in x)
+    | Lambda.Lam (x, e) -> Lambda.Lam (x, rename (List.filter (fun y -> y <> x) used) e)
+    | Lambda.App (l, r) -> Lambda.App (rename used l, rename used r)
+
+  let rec replace name target source used =
+    match target with
+    | Lambda.Id x ->
+      if x = name then source
+      else target
+    | Lambda.Lam (x, e) ->
+      if x = name then target
+      else Lambda.Lam (x, replace name e (rename (x::used) source) (x::used))
+    | Lambda.App (l, r) ->
+      Lambda.App (replace name l source used, replace name r source used)
+
+  let rec reduce : Lambda.lexp -> Lambda.lexp
+  = fun exp ->
+    match exp with
+    | Lambda.App ((Lambda.Lam (name, target), source)) ->
+      reduce (replace name target source [])
+    | Lambda.App (left, right) ->
+      Lambda.App (reduce left, reduce right)
+    | _ ->
+      exp
 
 end
