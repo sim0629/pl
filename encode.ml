73133d9cbd38440600ad79285b8cf92f13f6429b
sgm <sim0629@gmail.com>
2014-11-06 10:18:02 +0900

    encode.ml whitespace 정리

--- encode.ml
+++ encode.ml
@@ -1,14 +1,14 @@
 (*
- * SNU 4190.310 Programming Languages 
+ * SNU 4190.310 Programming Languages
  *
  * M0
  *)
 open M
-module Encoder = 
-  struct
-  	exception Error of string
+module Encoder =
+struct
+  exception Error of string
 
-	let encode : M.mexp -> Lambda.lexp
-	= fun pgm -> raise (Error "not implemented") (* Implement this *)
+  let encode : M.mexp -> Lambda.lexp
+  = fun pgm -> raise (Error "not implemented") (* Implement this *)
 
-  end
+end
