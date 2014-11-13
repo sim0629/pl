c96f40080c6d38a3235b1b4ccdf5ad84241c1c0e
sgm <sim0629@gmail.com>
2014-11-13 09:47:32 +0900

    Num 구현

--- encode.ml
+++ encode.ml
@@ -8,8 +8,24 @@ module Encoder =
 struct
   exception Error of string
 
+  let encode_num n =
+    let rec encode_num_in n result =
+      if n = 0 then
+        result
+      else if n < 0 then
+        raise (Error "negative integer")
+      else
+        encode_num_in (n - 1) (Lambda.App (Lambda.Id "f", result))
+    in
+    Lambda.Lam ("f",
+      Lambda.Lam ("x",
+        encode_num_in n (Lambda.Id "x")
+      )
+    )
+
   let encode : M.mexp -> Lambda.lexp
   = fun pgm -> match pgm with
+  | M.Num n -> encode_num n
   | _ -> raise (Error "not implemented") (* Implement this *)
 
 end
