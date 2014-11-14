871d3f1ab22cb7cd5f24fdfc22222d7af2557547
sgm <sim0629@gmail.com>
2014-11-14 17:49:56 +0900

    PUSH 구현

--- rozetta.ml
+++ rozetta.ml
@@ -6,7 +6,32 @@
 open Sm5
 open Sonata
 module Rozetta = struct
-  let rec trans : Sm5.command -> Sonata.command = fun command ->
-    Sonata.empty_command (* Implement this. *)
+
+  let rec trans_tail command result =
+    match command with
+    | [] -> result
+    | (Sm5.PUSH (Sm5.Val (Sm5.Z n)))::tail ->
+      trans_tail tail
+        ((Sonata.PUSH (Sonata.Val (Sonata.Z n)))::result)
+    | (Sm5.PUSH (Sm5.Val (Sm5.B b)))::tail ->
+      trans_tail tail
+        ((Sonata.PUSH (Sonata.Val (Sonata.B b)))::result)
+    | (Sm5.PUSH (Sm5.Val (Sm5.L l)))::tail ->
+      failwith "l"
+    | (Sm5.PUSH (Sm5.Val Sm5.Unit))::tail ->
+      trans_tail tail
+        ((Sonata.PUSH (Sonata.Val Sonata.Unit))::result)
+    | (Sm5.PUSH (Sm5.Val (Sm5.R r)))::tail ->
+      failwith "r"
+    | (Sm5.PUSH (Sm5.Id x))::tail ->
+      trans_tail tail
+        ((Sonata.PUSH (Sonata.Id x))::result)
+    | (Sm5.PUSH (Sm5.Fn (x, c)))::tail ->
+      let c' = trans_tail c Sonata.empty_command in
+      trans_tail tail
+        ((Sonata.PUSH (Sonata.Fn (x, c')))::result)
+
+  let trans : Sm5.command -> Sonata.command = fun command ->
+    trans_tail command Sonata.empty_command
 
 end
